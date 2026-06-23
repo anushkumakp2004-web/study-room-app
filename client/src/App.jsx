4ff303f736356cd4402ba508304876f504b4c805
anushkumakp2004-web <anushkumakp2004@gmail.com>
2026-06-23 12:11:18 +0530

    add landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Room from "./pages/Room";
 import CreateRoom from "./pages/CreateRoom";
-
+import Home from "./pages/Home";
 function App() {
   return (
     <Routes>
       <Route
   path="/"
+  element={<Home />}
+/>
+<Route
+  path="/create"
   element={<CreateRoom />}
 />
       <Route
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,44 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div>
-      <h1>Study Room App</h1>
+    <div
+  style={{
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    padding: "20px",
+  }}
+>
+      <h1>📚 Study Room</h1>
 
-      <p>Study together in real time.</p>
+<p>
+  Study together in real time with chat,
+  shared notes, and a collaborative
+  whiteboard.
+</p>
+<div style={{ marginBottom: "25px" }}>
+  <p>💬 Real-Time Chat</p>
+  <p>📝 Shared Notes</p>
+  <p>🎨 Collaborative Whiteboard</p>
+  <p>👥 Multi-User Rooms</p>
+</div>
 
-      <Link to="/room">
-        <button>Join Study Room</button>
+      <Link to="/create">
+<button
+  style={{
+    padding: "12px 24px",
+    borderRadius: "10px",
+    border: "none",
+    cursor: "pointer",
+    fontSize: "16px",
+    fontWeight: "bold",
+  }}
+>
+  Join Study Room
+</button>
       </Link>
     </div>
   );
