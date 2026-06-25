2d27dcaea79b86f15ce0088405ac5670bba212b9
anushkumakp2004-web <anushkumakp2004@gmail.com>
2026-06-25 20:21:32 +0530

    Fix router and add toast notifications

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import App from "./App";
 import "./index.css";
-import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <Toaster
-    position="top-right"
-    toastOptions={{
-      duration: 2000,
-      style: {
-        background: "#333",
-        color: "#fff",
-        borderRadius: "10px",
-      },
-    }}
-  />
+    <BrowserRouter>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 2000,
+          style: {
+            background: "#333",
+            color: "#fff",
+            borderRadius: "10px",
+          },
+        }}
+      />
 
-  <App />
-</React.StrictMode>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
